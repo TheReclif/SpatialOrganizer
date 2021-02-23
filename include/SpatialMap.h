80e781ce3aeec1c2e02f422df291da2662d6cd86
Marcel Chmielewski <17992687+TheReclif@users.noreply.github.com>
2021-02-23 22:43:25 +0100

    Added basic frame of SpatialMap; modified CMakeLists accordingly

---
+++ include/SpatialMap.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef SPATIALORGANIZER_SPATIALMAP_H
+#define SPATIALORGANIZER_SPATIALMAP_H
+
+#include <memory>
+
+namespace Spatial
+{
+	/// @brief TODO
+	/// @tparam Value TODO
+	/// @tparam Allocator TODO
+	template<class Value, class Allocator = std::allocator<char>>
+	class SpatialMap
+	{
+	public:
+		/// @brief Initializes the spatial map with a given cell size.
+		/// @param cellSize Cell size.
+		SpatialMap(const unsigned int cellSize);
+
+	private:
+
+	};
+}
+
+#endif
---
+++ src/SpatialMap.cpp
@@ -0,0 +1,1 @@
+#include "../include/SpatialMap.h"
